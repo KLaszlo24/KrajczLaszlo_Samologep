5f55db9d687dccd64e896dc2054ed5a648bb937d
KLaszlo24 <krajczlaci234@gmail.com>
2025-01-17 11:36:08 +0100

    Add project files.

---
+++ számológép/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace számológép
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+		}
+	}
+}
