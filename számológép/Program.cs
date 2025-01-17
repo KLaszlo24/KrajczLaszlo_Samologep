c3817a77951d555e3be5a5adade30c23142ca4af
KLaszlo24 <krajczlaci234@gmail.com>
2025-01-17 11:49:24 +0100

    Osszeadas_Kesz

--- számológép/Program.cs
+++ számológép/Program.cs
@@ -8,9 +8,25 @@ namespace számológép
 {
 	internal class Program
 	{
+		static int szam1 = 0;
+		static int szam2 = 0;
+
+
+
 		static void Main(string[] args)
 		{
+			Console.Write("Addj meg számot: ");
+			szam1 = Convert.ToInt32(Console.ReadLine());
+			Console.Write("Addj meg egy másik számot: ");
+			szam2 = Convert.ToInt32(Console.ReadLine());
 
+
+			Osszeadas();
 		}
+		static void Osszeadas()
+		{
+			int szam3 = szam1 + szam2;
+            Console.WriteLine($"Két szám összeadava: {szam3}");
+        }
 	}
 }
